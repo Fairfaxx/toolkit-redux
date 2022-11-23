6a74c29ab7525e6c93687f0e6b2d8cc2f14b90ad
Fede <53632624+Fairfaxx@users.noreply.github.com>
2022-11-23 17:38:33 -0300

    create the pokemon slice with actions, the pokemon view and all is already set (#2)
    Co-authored-by: FefeLemaire <federico.lemaire@alloy.digital>

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const PokemoApp = () => {
+  return (
+    <>
+      <h1>PokemoApp</h1>
+      <hr />
+      <ul>
+        <li>Hi</li>
+        <li>Hi</li>
+        <li>Hi</li>
+      </ul>
+    </>
+  );
+};
+
+export default PokemoApp;
