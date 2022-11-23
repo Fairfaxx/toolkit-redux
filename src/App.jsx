1b5b6f658b328b59e08c668092115f38c93b7515
FefeLemaire <federico.lemaire@alloy.digital>
2022-11-23 16:55:39 -0300

    counter slice created and added, increase & decrees & increase by the input value working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from './store/slices/counter';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [userNum, setUserNum] = useState(0);
+  console.log(userNum);
+  const { counter } = useSelector((state) => state.counter);
+  const { message } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
+  console.log(message);
   return (
     <div className="App">
       <div>
@@ -14,11 +25,23 @@ function App() {
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <input
+          type="number"
+          value={userNum}
+          placeholder="Add a number"
+          onChange={(e) => setUserNum(e.target.value)}
+        />
       </div>
-      <h1>Vite + React</h1>
+      <h1>{message && message}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => dispatch(increment())}>Increment</button>
+      </div>
+      <div className="card">
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>
+      <div className="card">
+        <button onClick={() => dispatch(incrementByAmount(userNum))}>
+          Increment by the value of the user
         </button>
       </div>
     </div>
