befcf416c8c8a177931d8addeb7d8fa40d6f3e2e
FefeLemaire <federico.lemaire@alloy.digital>
2022-11-23 21:00:29 -0300

    thunk created and is making the http request with a fetch and getting the pokemons arr

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getPokemons } from './store/slices/pokemon';
 
 const PokemoApp = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPokemons());
+  }, []);
   return (
     <>
       <h1>PokemoApp</h1>
