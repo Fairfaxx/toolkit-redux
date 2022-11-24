309a73e7048a46876dfe12a229d1ea19dc3d3b8c
Fede <53632624+Fairfaxx@users.noreply.github.com>
2022-11-23 21:24:23 -0300

    axios installed and created a new axios instance and already apply into the thunk function. Already displaying the names of pokemons in the screen (#4)
    Co-authored-by: FefeLemaire <federico.lemaire@alloy.digital>

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,13 +1,18 @@
+import { pokemonApi } from '../../../api/pokemonApi';
 import { startLoadingPokemons, setPokemons } from './pokemonSlice';
 
 export const getPokemons = (page = 0) => {
   return async (dispatch, getState) => {
     dispatch(startLoadingPokemons());
 
-    const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`
+    // const response = await fetch(
+    //   `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`
+    // );
+    // const data = await response.json();
+
+    const { data } = await pokemonApi.get(
+      `/pokemon?limit=10&offset=${page * 10}`
     );
-    const data = await response.json();
 
     dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
   };
